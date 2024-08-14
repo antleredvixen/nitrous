83c6ab68401c977ee5a4fdf9badb375c4d717b3c
Katie <antleredvixen@gmail.com>
2024-08-14 17:38:18 -0500

    Qt Project initialized

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_nitrous_FILE C:/Users/Katie/Code/antleredvixen/nitrous/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/nitrous.exe)
+set(__QT_DEPLOY_TARGET_nitrous_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_nitrous_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
